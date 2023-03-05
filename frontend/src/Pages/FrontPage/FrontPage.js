374723b1fd2eb4497dc54baf616cbf64ca8eb044
Jacob Enoch <79029984+JacobEnochCMSC471@users.noreply.github.com>
2023-03-05 10:37:41 -0500

    Added basic backend/api files, main python file, requirements files. For real this time.

---
+++ frontend/src/Pages/FrontPage/FrontPage.js
@@ -0,0 +1,12 @@
+import './FrontPage.css';
+
+function FrontPage() {
+  return (
+    <div className="FrontPage">
+      <header className="FrontPage-header">
+      </header>
+    </div>
+  );
+}
+
+export default FrontPage;
