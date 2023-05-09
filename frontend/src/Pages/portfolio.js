a973052e6d925fbd4b93509f933ea11b0a196823
Jacob Enoch <79029984+JacobEnochCMSC471@users.noreply.github.com>
2023-05-09 02:01:33 -0400

    added more to portfolio page

--- frontend/src/Pages/portfolio.js
+++ frontend/src/Pages/portfolio.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Background } from '../components/Background'
+import '../components/styles/portfolio/portfolio.css'
 
 const Portfolio = () => {
   return (
-    <div id='main-page'>
+    <div id='main-page-portfolio'>
       <div class="split left">
-
-      </div> 
-
-      <div class="split right">
-
+        <div class="centered">
+          <p> Metal Benders LLC Projects </p>
+        </div>
       </div>
 
-      
-      <Background>
-        
-      </Background>
-  </div>
+      <div class="split right">
+        <div class="centered">
+          <p> Programming Projects </p>
+        </div>
+      </div>
+      <Background />
+    </div>
   );
 };
 
