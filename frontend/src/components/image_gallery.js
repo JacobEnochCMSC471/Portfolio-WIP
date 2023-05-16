ee1b5ae5b4339bbf888acbc4616f6b72ceead197
Jacob Enoch <79029984+JacobEnochCMSC471@users.noreply.github.com>
2023-05-16 03:52:50 -0400

    Added portfolio images; will begin working on simple backend soon

---
+++ frontend/src/components/image_gallery.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+/* Not going to work - images need to be either imported individually or supplied from a backend server
+
+const base_path = '..src/images/mb_photos/'
+const base_photo_name = 'img_'
+
+var photo_paths = []
+
+for (let i = 0; i < 26; i++){
+    curr_photo = base_path + base_photo_name + i
+    photo_paths.push(curr_photo)
+}
+
+*/
+const ImageGallery = () => {
+
+    return (
+        <div class="gallery">
+
+        </div>
+    );
+}
+
+export default ImageGallery;
