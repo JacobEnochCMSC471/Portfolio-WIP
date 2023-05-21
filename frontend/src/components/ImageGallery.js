b8c7c9ce85f245bf618f3cfdb9be28bc5cbbfd0d
Jacob Enoch <79029984+JacobEnochCMSC471@users.noreply.github.com>
2023-05-21 03:30:36 -0400

    Cleaned up code a bit more

--- frontend/src/components/ImageGallery.js
+++ frontend/src/components/ImageGallery.js
@@ -1,36 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import axios from "axios"
+import React, { useState, useEffect } from 'react'
 
-/*
-const base_path = './public/images/mb_photos/'
-const base_photo_name = 'img_'
-const file_extension = '.jpg'
-
-var photo_paths = []
-
-for (let i = 0; i < 25; i++){
-    let curr_photo = base_path + base_photo_name + i + file_extension
-    photo_paths.push(curr_photo)
-}
-*/
+//Source: https://blog.miguelgrinberg.com/post/how-to-create-a-react--flask-project
 
 const ImageGallery = () => {
     
     const [currentData, setCurrentData] = useState(0);
     
+    // Fetch a single image from the FLask API - sent in Base64 
     useEffect(() => {
         fetch('/img_api').then(res => res.json()).then(data => {
             setCurrentData(data.img);
         });
     }, []);
 
-
     let base_str = 'data:image/png;base64, '
     let img_str = String(currentData)
     let img_src = base_str.concat(img_str)
-
-    console.log(img_src)
-    
     
     return (
         <div className="gallery">
